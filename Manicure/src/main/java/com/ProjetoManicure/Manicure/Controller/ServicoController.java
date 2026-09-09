f012ac3d02e0b9f59605c9a37887d5b04f123032
Fernando Felipe <fernandofelipe203@gmail.com>
2026-09-09 17:56:50 -0300

    v1.Finalizada

--- Manicure/src/main/java/com/ProjetoManicure/Manicure/Controller/ServicoController.java
+++ Manicure/src/main/java/com/ProjetoManicure/Manicure/Controller/ServicoController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//Fernando Felipe Alves De Sousa
 @RestController
 @RequestMapping("/servicos")
 @CrossOrigin(origins = "*")
